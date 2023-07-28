40259a58b9b275bddd880c63421bd4e954966334
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-28 13:22:09 -0400

    needed to add an import statement and the conditional 'if name' statement.

--- dict_pts.py
+++ dict_pts.py
@@ -1,3 +1,5 @@
+import sys
+
 points = {
     "a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7,
     "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14,
@@ -14,4 +16,8 @@ def dict_pts(points, words):
             point += points[char]
     return point
 
-print(dict_pts(points,words))
+def main():
+    print(dict_pts(points,words))
+
+if __name__ == "__main__":
+    main()
