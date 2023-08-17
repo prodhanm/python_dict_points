04b6b2d5afe80209d1330415015db3d16c9e6237
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-17 18:56:16 -0400

    added error handling to the code.

--- dict_pts.py
+++ dict_pts.py
@@ -1,5 +1,3 @@
-import sys
-
 points = {
     "a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7,
     "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14,
@@ -10,11 +8,16 @@ points = {
 words = "Geschwindigkeitsbegrenzung"
 
 def dict_pts(points, words):
-    point = 0
-    for char in words.lower():
-        if char in points:
-            point += points[char]
-    return point
+    try:
+        point = 0
+        for char in words.lower():
+            if char in points:
+                point += points[char]
+        return point
+    except (AttributeError,TypeError,ValueError) as err:
+        print(f"1. The variable words must be a string. \
+              2. Value can not be 'None' \
+              3. {err}")
 
 def main():
     print(dict_pts(points,words))
